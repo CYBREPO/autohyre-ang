3f0546ecf2c5c38041f5deac0a52492e409e2d1b
RoronoaZoro34 <shivanaik2398@gmail.com>
2023-06-16 01:09:08 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: "https://api.autohyre.com/api/",
+  url: "https://api.autohyre.com/",
   cacheKey: "login",
   encryptionKey: "8080808080808080",
   apiKey: "AIzaSyBG0EEFFVK5H0QIt-32cX32CGuhXWv3rSc"
